760baa4ca8d62453eddda2633b3c9fc79fb74cce
Martin Lloyd <martin.lloyd@thomsonreuters.com>
2015-12-02 09:56:27 -0500

    [ADD] Setup to enable a tvOS app.

---
+++ CarthageExample_tvOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarthageExample_tvOS
+//
+//  Created by Martin Lloyd on 02/12/2015.
+//  Copyright © 2015 Thomson Reuters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
